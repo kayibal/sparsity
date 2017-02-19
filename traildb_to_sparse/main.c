32ed45d2c8082030bb8cacf454005d9cf3949434
kayibal <alan.f.hoeng@gmail.com>
2017-02-19 15:43:05 +0100

    Initial Commit

---
+++ traildb_to_sparse/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  traildb_to_sparse
+//
+//  Created by Alan Höng on 19/02/2017.
+//  Copyright © 2017 Alan Höng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
