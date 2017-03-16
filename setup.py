e9fe3750137cb8e724221eb6f140057152029bd5
kayibal <alan.f.hoeng@gmail.com>
2017-03-17 00:17:49 +0100

    proper setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,19 @@ ext = Extension("sparsity._traildb",
                          'sparsity/src/hashtable.c',
                          'sparsity/src/linklist.c'],
                 include_dirs=['/usr/local/include/', np.get_include()],
-                install_requires=[
-                    'pandas>=0.19.2',
-                    'scipy>=0.18.1',
-                    'numpy>=1.12.0'
-                ],
                 libraries=["traildb"])
 
 setup(
+    name='sparsity',
+    version='0.1',
     ext_modules = cythonize([ext]),
+    author='Alan Hoeng',
+    author_email='alan.f.hoeng@gmail.com',
+    packages=['sparsity'],
+    install_requires=[
+                        'pandas>=0.19.2',
+                        'scipy>=0.18.1',
+                        'numpy>=1.12.0'
+                    ],
+    zip_safe=False
 )
