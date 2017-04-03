e41c0a0286cf2a8758f55db51593ca640fa0d89e
kayibal <alan.f.hoeng@gmail.com>
2017-04-03 15:17:36 +0200

    Make traildb optional for installing

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
-import numpy as np
 from distutils.core import setup, Extension
-
-from Cython.Build import cythonize
-
-ext = Extension("sparsity._traildb",
-                        ['sparsity/_traildb.pyx',
-                         'sparsity/src/traildb_coo.c',
-                         'sparsity/src/hashtable.c',
-                         'sparsity/src/linklist.c'],
-                include_dirs=['/usr/local/include/', np.get_include()],
-                libraries=["traildb"])
+try:
+    import numpy as np
+    from Cython.Build import cythonize
+    import traildb
+    ext = Extension("sparsity._traildb",
+                            ['sparsity/_traildb.pyx',
+                             'sparsity/src/traildb_coo.c',
+                             'sparsity/src/hashtable.c',
+                             'sparsity/src/linklist.c'],
+                    include_dirs=['/usr/local/include/', np.get_include()],
+                    libraries=["traildb"])
+    ext_modules = cythonize([ext])
+except ImportError:
+    ext_modules = None
 
 setup(
     name='sparsity',
     version='0.1',
-    ext_modules = cythonize([ext]),
+    ext_modules = ext_modules,
     author='Alan Hoeng',
     author_email='alan.f.hoeng@gmail.com',
     packages=['sparsity'],
