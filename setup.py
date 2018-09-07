7e2a9034e40d2cd601d44b520c8023fdc9e1b540
michcio1234 <michcio1234@gmail.com>
2018-09-07 17:14:45 +0200

    More info in setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,33 @@ from setuptools import find_packages
 packages = find_packages()
 packages.remove('sparsity.test')
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='sparsity',
     version=versioneer.get_version(),
     author='Alan Hoeng',
     author_email='alan.f.hoeng@gmail.com',
+    description="Sparse data processing toolbox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/datarevenue-berlin/sparsity",
     packages=packages,
     cmdclass=versioneer.get_cmdclass(),
     install_requires=[
-                        'pandas>=0.19.0,<=0.23.4',
-                        'scipy>=0.18.1',
-                        'numpy>=1.12.0',
-                        's3fs>=0.1.0',
-                    ],
-    test_requires=[
-        'moto'
+        'pandas>=0.19.0,<=0.23.4',
+        'scipy>=0.18.1',
+        'numpy>=1.12.0',
+        's3fs>=0.1.0',
     ],
-    zip_safe=False
+    test_requires=[
+        'moto',
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
 )
